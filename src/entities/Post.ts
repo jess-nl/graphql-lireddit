63b578329652ef35343842e1053965fc4cc85ac7
Jess Noriega-Lessard <jess@MacBook-Pro.local>
2021-01-28 18:49:53 -0500

    properly format files

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -2,16 +2,15 @@ import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
 
 @Entity()
 export class Post {
-
   @PrimaryKey()
   id!: number;
 
-  @Property({type: 'date'})
+  @Property({ type: "date" })
   createdAt = new Date();
 
-  @Property({ type: 'date', onUpdate: () => new Date() })
+  @Property({ type: "date", onUpdate: () => new Date() })
   updatedAt = new Date();
 
-  @Property({type: 'text'})
+  @Property({ type: "text" })
   title!: string;
 }
